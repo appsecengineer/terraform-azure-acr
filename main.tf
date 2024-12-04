3ec40be74b1f04c2557e33b02205a78ff99a2c7b
Kanumuru Rajesh <rajesh@we45.com>
2024-12-04 10:33:02 +0530

    added encryption realted stuff

--- main.tf
+++ main.tf
@@ -4,17 +4,6 @@ resource "azurerm_container_registry" "acr" {
   location            = var.location
   sku                 = var.sku
   admin_enabled       = var.admin_enabled
-}
-
-
-
-
-resource "azurerm_container_registry" "acr" {
-  name                = "${var.acr_name_prefix}${random_string.random.result}"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  sku                 = var.sku
-  admin_enabled       = var.admin_enabled
 
   dynamic "encryption" {
     for_each = var.enable_encryption && var.key_vault_key_id != null && var.identity_client_id != null ? [1] : []
