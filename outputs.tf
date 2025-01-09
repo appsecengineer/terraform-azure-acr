980c0ec049a03904d3274758bd436059d9b3c55a
Kanumuru Rajesh <72786996+kanumuruwe45@users.noreply.github.com>
2025-01-09 15:31:49 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,12 @@ output "acr_login_server" {
   description = "Login server URL of the Azure Container Registry"
   value       = azurerm_container_registry.acr.login_server
 }
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
